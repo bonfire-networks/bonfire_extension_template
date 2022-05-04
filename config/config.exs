4fd9bbccf3966a414b3dfb1279e36c2aa774892a
Mayel de Borniol <git@mayel.space>
2022-05-04 14:09:27 +1200

    Renamed modules

--- config/config.exs
+++ config/config.exs
@@ -16,7 +16,7 @@ import_config "bonfire_extension_template.exs"
 # You probably won't want to touch these. You might override some in
 # other config files.
 
-config :bonfire, :repo_module, Bonfire.Repo
+config :bonfire, :repo_module, Bonfire.Common.Repo
 
 config :phoenix, :json_library, Jason
 
@@ -30,8 +30,8 @@ config :mime, :types, %{
 
 config :bonfire_extension_template, :otp_app, :bonfire_extension_template
 config :bonfire_common, :otp_app, :bonfire_extension_template
-config :bonfire_extension_template, :repo_module, Bonfire.Repo
-config :bonfire_extension_template, ecto_repos: [Bonfire.Repo]
+config :bonfire_extension_template, :repo_module, Bonfire.Common.Repo
+config :bonfire_extension_template, ecto_repos: [Bonfire.Common.Repo]
 config :bonfire_extension_template, :localisation_path, "priv/localisation"
 config :bonfire_data_identity, Bonfire.Data.Identity.Credential, hasher_module: Argon2
 
--- lib/fake.ex
+++ lib/fake.ex
@@ -1,11 +1,4 @@
 defmodule Bonfire.ExtensionTemplate.Fake do
 
-  alias Bonfire.Me.Fake, as: MeFake
-
-  # import Bonfire.ExtensionTemplate.Integration
-
-  defdelegate fake_account!(attrs \\ %{}, opts \\ []), to: MeFake
-
-  defdelegate fake_user!(attrs \\ %{}, opts \\ []), to: MeFake
 
 end
