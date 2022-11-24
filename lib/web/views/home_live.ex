e1894991114ec148165919eb390a3fbfa260ab67
Mayel de Borniol <git@mayel.space>
2022-11-24 21:33:20 +1300

    Persistence

--- lib/web/views/home_live.ex
+++ lib/web/views/home_live.ex
@@ -25,4 +25,30 @@ defmodule Bonfire.ExtensionTemplate.Web.HomeLive do
        page_title: "ExtensionTemplate"
      )}
   end
+
+  def handle_params(params, uri, socket),
+    do:
+      Bonfire.UI.Common.LiveHandlers.handle_params(
+        params,
+        uri,
+        socket,
+        __MODULE__
+      )
+
+  def handle_info(info, socket),
+    do: Bonfire.UI.Common.LiveHandlers.handle_info(info, socket, __MODULE__)
+
+  def handle_event(
+        action,
+        attrs,
+        socket
+      ),
+      do:
+        Bonfire.UI.Common.LiveHandlers.handle_event(
+          action,
+          attrs,
+          socket,
+          __MODULE__,
+          &do_handle_event/3
+        )
 end
