fc35b60d34e7e428061b769bb3003711ce3521c1
Mayel de Borniol <git@mayel.space>
2024-02-11 19:22:50 +0000

    improve

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,6 @@
 import Config
 
-#### Basic configuration useful for tests, everything else should be in `Bonfire.ExtensionTemplate.RuntimeConfig`
+#### General configuration useful for tests, everything else should be in `bonfire_extension_template.exs` or `Bonfire.ExtensionTemplate.RuntimeConfig`
 
 # You probably won't want to touch these. You might override some in
 # other config files.
@@ -25,4 +25,5 @@ config :bonfire_extension_template, :localisation_path, "priv/localisation"
 
 config :bonfire_data_identity, Bonfire.Data.Identity.Credential, hasher_module: Argon2
 
+import_config "bonfire_extension_template.exs"
 # import_config "#{Mix.env()}.exs"
