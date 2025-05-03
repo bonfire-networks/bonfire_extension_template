9e306b6589bf5ba6da7bd5eac81d2c4eb1c6572d
Mayel de Borniol <git@mayel.space>
2025-05-03 20:08:04 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/1239

--- lib/integration.ex
+++ lib/integration.ex
@@ -1,7 +1,7 @@
 defmodule Bonfire.ExtensionTemplate do
   @moduledoc "./README.md" |> File.stream!() |> Enum.drop(1) |> Enum.join()
 
-  alias Bonfire.Common.Config
+  use Bonfire.Common.Config
   alias Bonfire.Common.Utils
   import Untangle
   import Bonfire.Common.Modularity.DeclareHelpers
